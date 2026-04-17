034a4e3032aa16fbf450ad4ae19c10fa8c622c2d
Robosword <32asingh@stratfordschools.net>
2026-04-16 20:41:56 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ from sklearn.model_selection import train_test_split
 import torch.nn
 import torch.functional.F
 df = pd.read_csv('chat_dataset.csv')
+
+def encodeSentiment(x):
+    if x == "neutral":
+        return 0
+    elif x == "negative":
+        return 1
+    else:
+        return 2
