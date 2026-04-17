475671bbc36b126ffc431eaf979fa8cdaaa7d9fd
Robosword <32asingh@stratfordschools.net>
2026-04-16 20:41:37 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import torch
 import pandas as pd
 import numpy as np
-from sklearn import train_test_split
+from sklearn.model_selection import train_test_split
 import torch.nn
 import torch.functional.F
+df = pd.read_csv('chat_dataset.csv')
